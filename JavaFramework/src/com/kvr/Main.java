c1a2fdba1bc3688eb86ef07c5535c50316e68638
kronson <kronson.dev@gmail.com>
2014-07-14 13:44:29 +0100

    Added some code

--- JavaFramework/src/com/kvr/Main.java
+++ JavaFramework/src/com/kvr/Main.java
@@ -1,7 +1,27 @@
 package com.kvr;
 
 public class Main {
+	
+	private Kev kev;
+	private String test = "Test";
+	
 	public static void main(String[] args) {
+		Kev kev = new Kev("Ronson");
+	}
+	
+	public void methodCall(String name, int age) {
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
+	}
+	
+	public static class Kev {
+		
+		private String surname;
+		
+		public Kev(String surname) {
+			this.surname = surname;
+		}
+		
 		
 	}
 }
